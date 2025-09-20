e80988b3a857cac129505f56e1823f69c8bb3c5c
sustatov027-max <sustatov027@gmail.com>
2025-09-20 15:23:05 +0300

    Add function create_logs_directory

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import atexit
 import json
 import logging.config
+from pathlib import Path
 
 from config import LogsConfig
 
@@ -18,8 +19,14 @@ def start_queue_handler(queue_handler: logging.Handler) -> None:
     queue_handler.listener.start()
     atexit.register(queue_handler.listener.stop)
 
+def create_logs_directory(logs_path: str="logs") -> None:
+    logs_dir = Path(logs_path)
+    if not logs_dir.exists():
+        logs_dir.mkdir(parents=True, exist_ok = True)
+        system_logger.info(f'Created logs directory" {logs_dir.absolute()}')
 
 def setup_logging() -> None:
+    create_logs_directory()
     config = load_log_config()
     logging.config.dictConfig(config)
     system_queue = logging.getHandlerByName("system_queue")
